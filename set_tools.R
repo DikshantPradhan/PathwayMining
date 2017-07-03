3d5a0fd1ac5200e14f5f2b48328469d32298a28c
Dikshant <dikshantpradhan@gmail.com>
2017-07-02 19:10:09 -0500

    laid down groundwork for some more predictor tools

--- set_tools.R
+++ set_tools.R
@@ -41,8 +41,70 @@ find_all_sets_for_rxn <- function(rxn_id, set_lists){
   }
 }
 
-correlation_predictor <- function(){
+correlation_predictor <- function(sample_df){ # need to complete this function
   #
+  cor_list <- rep(FALSE, 95)
+  cor_list[check_set_list_for_containing(c("CYTBD", "NADH16"), set_lists)] <- TRUE
+  
+  correlation_list <- c()
+  
+  sample_df = sampler(model, nPnts = 500)# ACHR(model,W,nPoints=nPnts,stepsPerPoint=10)
+  for (i in 1:nrow(sample_df)){
+    if (check_sets_for_containing(c("CYTBD", "NADH16"), og_set_list)){
+      correlation_list <- c(correlation_list, TRUE)
+    }
+    else {
+      correlation_list <- c(correlation_list, FALSE)
+    }
+  }
+  
+  
+  report <- paste(i, "::", "sample_df:", nrow(sample_df), ";", "corr:", length(correlation_list))
+  
+  for (i in 1:95){
+    sample_df2 = sampler(suppressed_model(model, i), nPnts = 500)
+    sample_df <- rbind.data.frame(sample_df, sample_df2)
+    for (j in 1:(nrow(sample_df) - length(correlation_list))){
+      correlation_list <- c(correlation_list, cor_list[i])
+    }
+    report <- c(report, paste(i, "::", "sample_df:", nrow(sample_df), ";", "corr:", length(correlation_list)))
+  }
+  sample_df <- data.frame(sample_df)
+  fit <- tree(correlation_list ~ ., data = sample_df)
+  
+  plot(fit)
+  text(fit)
+  return(fit)
+}
+
+predictor_matrix_generator <- function(){ # need to complete this function
+  
+  sample_df <- c() # fill this in
+  predictors <- c()
+  
+  for (i in 1:95){
+    for (j in i:95){
+      predictors[i,j] <- correlation_predictor()
+    }
+  }
+  
+  return(predictors)
+}
+
+pair_lists_from_predictors <- function(predictors){
+  
+  pairs <- c()
+  
+  for (i in 1:95){
+    for (j in i:95){
+      prediction <- c()
+      if (prediction){
+        # add pair to list
+      }
+    }
+  }
+  
+  return(pairs)
 }
 
 cor_list <- rep(FALSE, 95)
@@ -60,11 +122,13 @@ for (i in 1:nrow(sample_df)){
   }
 }
 
+sample_df["block"] <- rep(0, nrow(sample_df))
 
 report <- paste(i, "::", "sample_df:", nrow(sample_df), ";", "corr:", length(correlation_list))
 
 for (i in 1:95){
   sample_df2 = sampler(suppressed_model(model, i), nPnts = 500)
+  sample_df2["block"] <- rep(i, nrow(sample_df2))
   sample_df <- rbind.data.frame(sample_df, sample_df2)
   for (j in 1:(nrow(sample_df) - length(correlation_list))){
     correlation_list <- c(correlation_list, cor_list[i])
