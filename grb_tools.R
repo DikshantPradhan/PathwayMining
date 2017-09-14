2301bcf54f2bf62d4cb50e8f29b38762c6f8f162
Dikshant Pradhan <dikshantpradhan@gmail.com>
2017-09-14 15:26:37 -0500

    still dealing with too much coupling

--- grb_tools.R
+++ grb_tools.R
@@ -47,20 +47,21 @@ GRB_generate_pair_lists <- function(model, suppression_idxs){
 
   for (i in suppression_idxs){
 
-    prev_ub <- model$getattr("UB")[vars[i]]
-    prev_lb <- model$getattr("LB")[vars[i]]
+    #prev_ub <- model$getattr("UB")[vars[i]]
+    #prev_lb <- model$getattr("LB")[vars[i]]
 
-    #model <- GRB_ecoli_model()
+    model <- GRB_ecoli_model()
 
     # block i
     model$setattr("UB", setNames(0, vars[i]))
     model$setattr("LB", setNames(0, vars[i]))
 
-    pair_lists[i] <- list(return_couples(flux_coupling_raptor(model)$coupled))
+    #pair_lists[i] <- list(return_couples(flux_coupling_raptor(model)$coupled))
+    pair_lists[i] <- list(return_couples(raptor::flux_coupling(model)$coupled))
 
     # unfix i
-    model$setattr("UB", prev_ub)
-    model$setattr("LB", prev_lb)
+    #model$setattr("UB", prev_ub)
+    #model$setattr("LB", prev_lb)
   }
   return(pair_lists)
 }
@@ -73,10 +74,10 @@ GRB_generate_set_lists <- function(model, suppression_idxs){
 
   for (i in suppression_idxs){
 
-    prev_ub <- model$getattr("UB")[vars[i]]
-    prev_lb <- model$getattr("LB")[vars[i]]
+    #prev_ub <- model$getattr("UB")[vars[i]]
+    #prev_lb <- model$getattr("LB")[vars[i]]
 
-    #model <- GRB_ecoli_model()
+    model <- GRB_ecoli_model()
 
     # block i
     model$setattr("UB", setNames(0, vars[i]))
@@ -85,8 +86,8 @@ GRB_generate_set_lists <- function(model, suppression_idxs){
     set_lists[i] <- list(get_list_of_sets(return_couples(flux_coupling_raptor(model)$coupled)))
 
     # unfix i
-    model$setattr("UB", prev_ub)
-    model$setattr("LB", prev_lb)
+    #model$setattr("UB", prev_ub)
+    #model$setattr("LB", prev_lb)
 
   }
 
--- main_remote.R
+++ main_remote.R
@@ -6,18 +6,22 @@ source('~/GitHub/PathwayMining/set_tools.R')
 source('~/GitHub/PathwayMining/raptor_coupling.R')
 source('~/GitHub/PathwayMining/grb_tools.R')
 
-#source('~/GitHub/PathwayMining/load_mod.R')
+source('~/GitHub/PathwayMining/load_mod.R')
 
+rxns <- c()
 
-#for (i in 1:length(ecoli_set_lists)){
-#  for (j in 1:length(ecoli_set_lists[[i]])){
-#    check <- check_sets_for_containing(ecoli_set_lists[[i]][[j]], set_lists[[i]])
-#    if (!check){
-#      print(c(i,j))
-#      print(ecoli_set_lists[[i]][[j]])
-#    }
-#  }
-#}
+for (i in 1:length(ecoli_set_lists)){
+  test_set <- get_list_of_sets(ecoli_pair_lists[[i]])
+
+  for (j in 1:length(test_set)){
+    check <- check_sets_for_containing(test_set[[j]], set_lists[[i]])
+    if (!check){
+      print(c(i,j))
+      print(test_set[[j]])
+      rxns <- c(rxns, unlist(test_set[[j]]))
+    }
+  }
+}
 
 #for (i in 1:length(ecoli_r1_set)){
 #  check <- check_sets_for_containing(union_set_list[[i]], ecoli_r1_set)
@@ -29,9 +33,17 @@ source('~/GitHub/PathwayMining/grb_tools.R')
 #  print(length(ecoli_set_lists[[i]]))
 #}
 
-for (i in 1:length(ecoli_pair_lists)){
-  print(c(length(ecoli_pair_lists[[i]]), length(ecoli_pair_lists_2[[i]])))
+#for (i in 1:length(ecoli_pair_lists)){
+  #print(c(length(ecoli_pair_lists[[i]]), length(ecoli_pair_lists_2[[i]])))
+#  test_set <- get_list_of_sets(ecoli_pair_lists[[i]])
+#  print(c(length(test_set), length(ecoli_set_lists[[i]])))
+  #for (j in 1:length(test_set)){
+  #  print(c(length(test_set[[j]]), length(ecoli_set_lists[[i]][[j]])))
+  #  #for (k in 1:length(test_set[[i]])){
+  #  #  if (!all.equal(test_set[[i]][j], ecoli_set_lists[[i]][j])){print(c(i,j))}
+  #  #}
+  #}
   #for (j in 1:length(ecoli_pair_lists[[i]])){
   #  if (!all.equal(ecoli_pair_lists_2[[i]][j,],ecoli_pair_lists_2[[i]][j,])){print(c(i,j))}
   #}
-}
+#}
